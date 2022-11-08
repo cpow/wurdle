52829791f6c711c6a207f4bc057b12455034bd07
Chris Power <chris@browntreelabs.com>
2022-11-08 13:06:25 -0500

    Add animations, and colors for already guessed items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import './App.css';
+import './App.scss';
 import {useState} from "react";
 import {useKey} from "react-use";
+import styled from "styled-components";
 
 function App() {
   const [currentRow, setCurrentRow] = useState(1);
@@ -26,7 +27,7 @@ function App() {
 
     if (currentColumn <= 5) {
       const newGuesses = [...guesses];
-      newGuesses.push({key, row: currentRow, column: currentColumn});
+      newGuesses.push({key, guessType: 'none', row: currentRow, column: currentColumn});
 
       setGuesses(newGuesses);
       setCurrentColumn(currentColumn + 1);
@@ -43,7 +44,23 @@ function App() {
     setCurrentColumn(currentColumn - 1);
   }
 
-  function checkWinnerAndMoveDown() {
+  function guessForRow(row) {
+    const newGuesses = [...guesses].map((guess) => {
+      if (guess.row === row) {
+        if (word.at(guess.column - 1) === guess.key) {
+          return {...guess, guessType: 'correct'};
+        } else if (word.includes(guess.key)) {
+          return {...guess, guessType: 'partial'};
+        } else {
+          return {...guess, guessType: 'incorrect'};
+        }
+      } else {
+        return {...guess};
+      }
+    });
+
+    setGuesses(newGuesses);
+
     const wordFromGuesses = guesses.reduce((acc, guess) => {
       if (guess.row === currentRow) {
         acc.push(guess.key);
@@ -57,25 +74,65 @@ function App() {
     if (wordFromGuesses === word) {
       setWinner(true);
     }
+  }
+
+  function checkWinnerAndMoveDown() {
+    // const wordFromGuesses = guesses.reduce((acc, guess) => {
+    //   if (guess.row === currentRow) {
+    //     acc.push(guess.key);
+    //     return acc;
+    //   } else {
+    //     return acc;
+    //   }
+    // }, []).join('');
+    //
+    //
+    // if (wordFromGuesses === word) {
+    //   setWinner(true);
+    // }
 
     if (currentRow < 6) {
       setCurrentColumn(1);
+      guessForRow(currentRow);
       setCurrentRow(currentRow + 1);
     }
   }
 
   function GridItem({coords}) {
-    const text = guesses.map((guess) => {
-      if (guess.row === coords[0] && guess.column === coords[1]) {
-        return guess.key;
-      }
+    const matchingGuess = guesses.find((guess) => {
+      return (guess.row === coords[0] && guess.column === coords[1]);
     });
 
+    const text = matchingGuess ? matchingGuess.key : '';
+
+    if (matchingGuess && matchingGuess.guessType !== 'none') {
+      if (currentRow - 1 === matchingGuess.row  && guesses[guesses.length - 1].row === matchingGuess.row) {
+        return (
+          <AlreadyGuessedGridItem guessType={matchingGuess.guessType}>
+            {text}
+          </AlreadyGuessedGridItem>
+        );
+      }
+      return (
+        <GuessedGridItem guessType={matchingGuess.guessType}>
+          {text}
+        </GuessedGridItem>
+      );
+    }
+
+    if (coords[0] === currentRow && coords[1] === currentColumn - 1) {
+      return (
+        <ActiveGridItem>
+          {text}
+        </ActiveGridItem>
+      );
+    }
+
     return (
-      <div className="gridItem">
+      <BaseGridItem>
         {text}
-      </div>
-    )
+      </BaseGridItem>
+    );
   }
 
   function Row({row}) {
@@ -84,7 +141,7 @@ function App() {
         {[1,2,3,4,5].map((column) => {
           return (
             <GridItem coords={[row, column]} />
-          )
+          );
         })}
       </div>
     )
@@ -106,5 +163,71 @@ function App() {
     </div>
   );
 }
+
+const BaseGridItem = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 60px;
+  height: 60px;
+  font-weight: bold;
+  font-size: 24px;
+  text-transform: capitalize;
+  border: 2px solid #D3D3D3;
+`;
+
+const  AlreadyGuessedGridItem = styled(BaseGridItem)`
+  color: white;
+  background: ${(props) => {
+    console.log(props);
+    if (props.guessType === 'correct') {
+      return '#75975e';
+    }
+    if (props.guessType === 'partial') {
+      return '#e6b400';
+    }
+    return '#36454F';
+  }};
+
+  @keyframes flip {
+    0% {
+      transform: perspective(800px) rotateY(0);
+      animation-timing-function: ease-out;
+    }
+    40% {
+      transform: perspective(800px) translateZ(150px) rotateY(170deg);
+      animation-timing-function: ease-out;
+    }
+    50% {
+      transform: perspective(800px) translateZ(150px) rotateY(190deg) scale(1);
+      animation-timing-function: ease-in;
+    }
+    80% {
+      transform: perspective(800px) rotateY(360deg) scale(.95);
+      animation-timing-function: ease-in;
+    }
+    100% {
+      transform: perspective(800px) scale(1);
+      animation-timing-function: ease-in;
+    }
+  }
+
+  animation-name: flip;
+  animation-duration: 0.5s;
+`;
+
+const GuessedGridItem = styled(AlreadyGuessedGridItem)`
+  animation: none;
+`;
+
+const  ActiveGridItem = styled(BaseGridItem)`
+  @keyframes pop-in {
+    0% {}
+    50% {border: 4px solid #282c34;}
+    100% {border: 2px solid #282c34;}
+  }
+  animation-name: pop-in;
+  animation-duration: .25s;
+`;
 
 export default App;
